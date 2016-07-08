7231cab438a7b896edf76eed8d980804c528d056
kazuki nozawa <kazuki.nozawa@mbp.intra.emercury.co.jp>
2016-07-08 12:30:21 +0900

    fix plugin file name

--- lib/fluent/plugin/filter_cf_query.rb
+++ lib/fluent/plugin/filter_cf_query.rb
@@ -1,5 +1,5 @@
 class Fluent::CfQueryFilter < Fluent::Filter
-  Fluent::Plugin.register_filter('cf_query_filter', self)
+  Fluent::Plugin.register_filter('cf_query', self)
   def filter(tag, time, record)
     return nil unless record['day'].start_with?('2')
     Hash[URI.decode_www_form(record['cs_uri_query'])]
