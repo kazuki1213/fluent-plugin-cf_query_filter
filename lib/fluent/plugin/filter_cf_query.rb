4a0d3e879753afd409d04b27fd684a83811ae518
kazuki nozawa <kazuki.nozawa@mbp.intra.emercury.co.jp>
2016-07-12 16:23:20 +0900

    [modify] Support ASCII

--- lib/fluent/plugin/filter_cf_query.rb
+++ lib/fluent/plugin/filter_cf_query.rb
@@ -1,7 +1,8 @@
+require 'rack/utils'
 class Fluent::CfQueryFilter < Fluent::Filter
   Fluent::Plugin.register_filter('cf_query', self)
   def filter(tag, time, record)
     return nil unless record['date'].start_with?('2')
-    Hash[URI.decode_www_form(record['cs-uri-query'])]
+    Rack::Utils.parse_nested_query(record['cs-uri-query'])
   end
 end
